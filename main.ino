706eadf860ce4551a4bae4086d3866dba2a59e44
Dominik Lastovcic <dominik.lastovcic@gmail.com>
2025-11-09 18:55:39 +0100

    bugfixing

--- main.ino
+++ main.ino
@@ -365,7 +365,7 @@ class MyCallbacks : public BLECharacteristicCallbacks
       {
         response_array[0] = 0x01;
         response_array[1] = 0x00;
-        response_array[2] = 0x0E;
+        response_array[2] = 0x10;
         response_array[3] = 0x00;
         response_array[4] = 0x01;
         response_array[5] = 0x00;
@@ -469,10 +469,10 @@ class MyCallbacks : public BLECharacteristicCallbacks
         response_array[3] = 0x00;
         response_array_size = 4;
         // check if page exists
-        if (write_value[2] > 0 && write_value[2] <= max_pages)
+        if (write_value[2] >= 0 && write_value[2] < max_pages)
         {
           // if correct number of values
-          if (write_value[3] > 1 && write_value[3] < 9)
+          if (write_value[3] >= 0 && write_value[3] < pages[write_value[2]].getPageValueCnt())
           {
             pages[write_value[2]].setMaxPageValCnt(write_value[3]);
           }
@@ -503,7 +503,7 @@ class MyCallbacks : public BLECharacteristicCallbacks
         response_array[3] = 0x00;
         response_array_size = 4;
         // check if page exists
-        if (write_value[2] > 0 && write_value[2] <= max_pages)
+        if (write_value[2] >= 0 && write_value[2] < max_pages)
         {
           // check if layout exists
           if (write_value[3] == CRITICAL_INFO_LAYOUT || write_value[3] == NON_CRITICAL_INFO_LAYOUT)
@@ -534,10 +534,10 @@ class MyCallbacks : public BLECharacteristicCallbacks
         response_array[3] = 0x00;
         response_array_size = 4;
         // check if page exists
-        if (write_value[2] > 0 && write_value[2] <= max_pages)
+        if (write_value[2] >= 0 && write_value[2] < max_pages)
         {
           // check if value exists
-          if (write_value[3] > 0 && write_value[3] <= pages[write_value[2]].getPageValueCnt())
+          if (write_value[3] >= 0 && write_value[3] < pages[write_value[2]].getPageValueCnt())
           {
             // check digit number
             if (write_value[4] > 0 && write_value[4] < 4)
@@ -580,10 +580,10 @@ class MyCallbacks : public BLECharacteristicCallbacks
         response_array[3] = 0x00;
         response_array_size = 4;
         // check if page exists
-        if (write_value[2] >= 0 && write_value[2] <= max_pages)
+        if (write_value[2] >= 0 && write_value[2] < max_pages)
         {
           // check if value exists
-          if (write_value[3] > 0 && write_value[3] <= pages[write_value[2]].getPageValueCnt())
+          if (write_value[3] >= 0 && write_value[3] < pages[write_value[2]].getPageValueCnt())
           {
             // check if string too long
             if (write_value[4] < MAX_STR_LEN - 1)
@@ -620,10 +620,10 @@ class MyCallbacks : public BLECharacteristicCallbacks
         response_array[3] = 0x00;
         response_array_size = 4;
         // check if page exists
-        if (write_value[2] >= 0 && write_value[2] <= max_pages)
+        if (write_value[2] >= 0 && write_value[2] < max_pages)
         {
           // check if value exists
-          if (write_value[3] > 0 && write_value[3] <= pages[write_value[2]].getPageValueCnt())
+          if (write_value[3] >= 0 && write_value[3] < pages[write_value[2]].getPageValueCnt())
           {
             // check if string too long
             if (write_value[4] < MAX_STR_LEN - 1)
@@ -660,10 +660,10 @@ class MyCallbacks : public BLECharacteristicCallbacks
         response_array[3] = 0x00;
         response_array_size = 4;
         // check if page exists
-        if (write_value[2] >= 0 && write_value[2] <= max_pages)
+        if (write_value[2] >= 0 && write_value[2] < max_pages)
         {
           // check if value exists
-          if (write_value[3] > 0 && write_value[3] <= pages[write_value[2]].getPageValueCnt())
+          if (write_value[3] >= 0 && write_value[3] < pages[write_value[2]].getPageValueCnt())
           {
             if (write_value[4] == 0)
               if (write_value[5] == 255)
@@ -793,10 +793,10 @@ class MyCallbacks : public BLECharacteristicCallbacks
         response_array[3] = 0x00;
         response_array_size = 4;
         // check if page exists
-        if (write_value[2] >= 0 && write_value[2] <= max_pages)
+        if (write_value[2] >= 0 && write_value[2] < max_pages)
         {
           // check if value exists
-          if (write_value[3] > 0 && write_value[3] <= pages[write_value[2]].getPageValueCnt())
+          if (write_value[3] >= 0 && write_value[3] < pages[write_value[2]].getPageValueCnt())
           {
             temp += write_value[4] << 0;
             temp += write_value[5] << 8;
@@ -1113,7 +1113,7 @@ void setup()
   // pages[0].values[3].setDesc("Right", 5);
   // pages[0].values[3].setUnit("C", 1);
   // pages[0].values[4].setValue(0);
-  // pages[0].values[4].setDesc("Boots", 5);
+  pages[0].values[4].setDesc("Boots", 5);
   // pages[0].values[4].setUnit("", 0);
   // pages[1].values[0].setValue(31);
   // pages[1].values[0].setDesc("Temp Air", 8);
@@ -1127,7 +1127,7 @@ void setup()
   // pages[1].values[3].setValue(67);
   // pages[1].values[3].setDesc("Heart rate", 10);
   // pages[1].values[3].setUnit("BPM", 3);
-  pages[1].setLayoutType(NON_CRITICAL_INFO_LAYOUT);
+  // pages[1].setLayoutType(NON_CRITICAL_INFO_LAYOUT);
 
   // Get watch instance
   ttgo = TTGOClass::getWatch();
@@ -1254,42 +1254,43 @@ void setup()
   sensor->enableWakeupInterrupt();
 
   ttgo->setBrightness(brightness_level);
-  tft->fillScreen(TFT_BLACK);
 
-  ttgo->lvgl_begin();
+  // ttgo->lvgl_begin();
 
-  lv_obj_t *text = lv_label_create(lv_scr_act(), NULL);
-  lv_label_set_text(text, "PDM Microphone Test");
-  lv_obj_align(text, NULL, LV_ALIGN_IN_TOP_MID, 0, 20);
+  // lv_obj_t *text = lv_label_create(lv_scr_act(), NULL);
+  // lv_label_set_text(text, "PDM Microphone Test");
+  // lv_obj_align(text, NULL, LV_ALIGN_IN_TOP_MID, 0, 20);
 
-  chart = lv_chart_create(lv_scr_act(), NULL);
-  lv_obj_set_size(chart, 200, 150);
-  lv_obj_align(chart, NULL, LV_ALIGN_CENTER, 0, 0);
-  lv_chart_set_type(chart, LV_CHART_TYPE_LINE); /*Show lines and points too*/
-  lv_chart_set_range(chart, 0, 800);
+  // chart = lv_chart_create(lv_scr_act(), NULL);
+  // lv_obj_set_size(chart, 200, 150);
+  // lv_obj_align(chart, NULL, LV_ALIGN_CENTER, 0, 0);
+  // lv_chart_set_type(chart, LV_CHART_TYPE_LINE); /*Show lines and points too*/
+  // lv_chart_set_range(chart, 0, 800);
 
-  ser1 = lv_chart_add_series(chart, LV_COLOR_RED);
+  // ser1 = lv_chart_add_series(chart, LV_COLOR_RED);
 
-  i2s_config_t i2s_config = {
-      .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX | I2S_MODE_PDM),
-      .sample_rate = 44100,
-      .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
-      .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
-      .communication_format = (i2s_comm_format_t)(I2S_COMM_FORMAT_I2S | I2S_COMM_FORMAT_I2S_MSB),
-      .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
-      .dma_buf_count = 2,
-      .dma_buf_len = 128,
-  };
+  // i2s_config_t i2s_config = {
+  //     .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX | I2S_MODE_PDM),
+  //     .sample_rate = 44100,
+  //     .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
+  //     .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
+  //     .communication_format = (i2s_comm_format_t)(I2S_COMM_FORMAT_I2S | I2S_COMM_FORMAT_I2S_MSB),
+  //     .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
+  //     .dma_buf_count = 2,
+  //     .dma_buf_len = 128,
+  // };
 
-  i2s_pin_config_t i2s_cfg;
-  i2s_cfg.bck_io_num = I2S_PIN_NO_CHANGE;
-  i2s_cfg.ws_io_num = MIC_CLOCK;
-  i2s_cfg.data_out_num = I2S_PIN_NO_CHANGE;
-  i2s_cfg.data_in_num = MIC_DATA;
+  // i2s_pin_config_t i2s_cfg;
+  // i2s_cfg.bck_io_num = I2S_PIN_NO_CHANGE;
+  // i2s_cfg.ws_io_num = MIC_CLOCK;
+  // i2s_cfg.data_out_num = I2S_PIN_NO_CHANGE;
+  // i2s_cfg.data_in_num = MIC_DATA;
 
-  i2s_driver_install(I2S_NUM_0, &i2s_config, 0, NULL);
-  i2s_set_pin(I2S_NUM_0, &i2s_cfg);
-  i2s_set_clk(I2S_NUM_0, 44100, I2S_BITS_PER_SAMPLE_16BIT, I2S_CHANNEL_MONO);
+  // i2s_driver_install(I2S_NUM_0, &i2s_config, 0, NULL);
+  // i2s_set_pin(I2S_NUM_0, &i2s_cfg);
+  // i2s_set_clk(I2S_NUM_0, 44100, I2S_BITS_PER_SAMPLE_16BIT, I2S_CHANNEL_MONO);
+  // tft->fillScreen(TFT_BLACK);
+
 }
 
 void loop()
@@ -1476,53 +1477,53 @@ void loop()
     ttgo->power->clearIRQ();
   }
   // MICROPHONE AND CHART
-  {
-    read_len = 0;
-    j = j + 1;
-    i2s_read(I2S_NUM_0, (char *)buffer, BUFFER_SIZE, &read_len, portMAX_DELAY);
-    for (int i = 0; i < BUFFER_SIZE / 2; i++)
-    {
-      val1 = buffer[i * 2];
-      val2 = buffer[i * 2 + 1];
-      val16 = val1 + val2 * 256;
-      if (val16 > 0)
-      {
-        val_avg = val_avg + val16;
-        val_max = max(val_max, val16);
-      }
-      if (val16 < 0)
-      {
-        val_avg_1 = val_avg_1 + val16;
-        val_max_1 = min(val_max_1, val16);
-      }
-      all_val_avg = all_val_avg + val16;
-      if (abs(val16) >= 20)
-        all_val_zero1 = all_val_zero1 + 1;
-      if (abs(val16) >= 15)
-        all_val_zero2 = all_val_zero2 + 1;
-      if (abs(val16) > 5)
-        all_val_zero3 = all_val_zero3 + 1;
-    }
+  // {
+  //   read_len = 0;
+  //   j = j + 1;
+  //   i2s_read(I2S_NUM_0, (char *)buffer, BUFFER_SIZE, &read_len, portMAX_DELAY);
+  //   for (int i = 0; i < BUFFER_SIZE / 2; i++)
+  //   {
+  //     val1 = buffer[i * 2];
+  //     val2 = buffer[i * 2 + 1];
+  //     val16 = val1 + val2 * 256;
+  //     if (val16 > 0)
+  //     {
+  //       val_avg = val_avg + val16;
+  //       val_max = max(val_max, val16);
+  //     }
+  //     if (val16 < 0)
+  //     {
+  //       val_avg_1 = val_avg_1 + val16;
+  //       val_max_1 = min(val_max_1, val16);
+  //     }
+  //     all_val_avg = all_val_avg + val16;
+  //     if (abs(val16) >= 20)
+  //       all_val_zero1 = all_val_zero1 + 1;
+  //     if (abs(val16) >= 15)
+  //       all_val_zero2 = all_val_zero2 + 1;
+  //     if (abs(val16) > 5)
+  //       all_val_zero3 = all_val_zero3 + 1;
+  //   }
 
-    if (j % 2 == 0 && j > 0)
-    {
-      val_avg = val_avg / BUFFER_SIZE;
-      val_avg_1 = val_avg_1 / BUFFER_SIZE;
-      all_val_avg = all_val_avg / BUFFER_SIZE;
+  //   if (j % 2 == 0 && j > 0)
+  //   {
+  //     val_avg = val_avg / BUFFER_SIZE;
+  //     val_avg_1 = val_avg_1 / BUFFER_SIZE;
+  //     all_val_avg = all_val_avg / BUFFER_SIZE;
 
-      lv_chart_set_next(chart, ser1, val_avg);
+  //     lv_chart_set_next(chart, ser1, val_avg);
 
-      val_avg = 0;
-      val_max = 0;
+  //     val_avg = 0;
+  //     val_max = 0;
 
-      val_avg_1 = 0;
-      val_max_1 = 0;
+  //     val_avg_1 = 0;
+  //     val_max_1 = 0;
 
-      all_val_avg = 0;
-      all_val_zero1 = 0;
-      all_val_zero2 = 0;
-      all_val_zero3 = 0;
-    }
-    lv_task_handler();
-  }
+  //     all_val_avg = 0;
+  //     all_val_zero1 = 0;
+  //     all_val_zero2 = 0;
+  //     all_val_zero3 = 0;
+  //   }
+  //   lv_task_handler();
+  // }
 }
